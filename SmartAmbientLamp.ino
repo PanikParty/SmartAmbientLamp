a711a0d0f31e00e73525e135df8ad7b722cf59f1
Frank <swalmen.angelino@gmail.com>
2022-01-18 10:03:42 -0800

    Initial Commit

---
+++ SmartAmbientLamp.ino
@@ -0,0 +1,590 @@
+#include "config.h"
+
+int sensorPin = A5;    // select the input pin for the potentiometer
+int ledPin = 13;      // select the pin for the LED
+
+// software SPI
+Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS, LIS3DH_MOSI, LIS3DH_MISO, LIS3DH_CLK);
+// hardware SPI
+//Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS);
+// I2C
+//Adafruit_LIS3DH lis = Adafruit_LIS3DH();
+
+sensors_event_t getAccelData () {
+  lis.read();      // get X Y and Z data at once
+  // Then print out the raw data
+//  Serial.print("X:  "); Serial.print(lis.x);
+//  Serial.print("  \tY:  "); Serial.print(lis.y);
+//  Serial.print("  \tZ:  "); Serial.print(lis.z);
+
+  /* Or....get a new sensor event, normalized */
+  sensors_event_t event;
+  lis.getEvent(&event);
+
+  /* Display the results (acceleration is measured in m/s^2) */
+  Serial.print("\t\tX: "); Serial.print(event.acceleration.x);
+  Serial.print(" \tY: "); Serial.print(event.acceleration.y);
+  Serial.print(" \tZ: "); Serial.print(event.acceleration.z);
+//  Serial.println(" m/s^2 ");
+
+  Serial.println();
+  return event;  
+}
+
+sensors_event_t getAmbientConditions () {
+  // read the value from the sensor; scale it to max brightness 0-255 (256/1024)
+  int sensorValue = analogRead(sensorPin)/4;
+  // Serial.print(str+"[Sensor] A5 "+sensorValue+"...\n");
+  strip.setBrightness(sensorValue+5); // Set BRIGHTNESS (min = 5)
+
+  return getAccelData ();
+//  clickDetect ();  
+}
+
+void stripOff () {
+  strip.clear ();
+  strip.show ();  
+}
+
+uint8_t colorZap(uint32_t color, int wait) {
+  for(int i=0; i<LED_COUNT; i++) { // For each pixel in strip...
+    strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
+  }
+  strip.show();                          //  Update strip to match
+  delay(wait);                           //  Pause for a moment
+  sensors_event_t  event = getAmbientConditions ();
+  if (event.acceleration.z < 0){
+    stripOff ();
+    return 0;
+  }
+  
+  return 1;
+}
+
+uint8_t colorFill (uint32_t color, int wait) {
+  
+}
+// Fill strip pixels one after another with a color. Strip is NOT cleared
+// first; anything there will be covered pixel by pixel. Pass in color
+// (as a single 'packed' 32-bit value, which you can get by calling
+// strip.Color(red, green, blue) as shown in the loop() function above),
+// and a delay time (in milliseconds) between pixels.
+uint8_t colorWipe(uint32_t color, int wait) {
+  for(int i=0; i<LED_COUNT; i++) { // For each pixel in strip...
+    strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
+    strip.show();                          //  Update strip to match
+    delay(wait);                           //  Pause for a moment
+    sensors_event_t  event = getAmbientConditions ();
+    if (event.acceleration.z < 0){
+      stripOff ();
+      return 0;
+    }
+  }
+  return 1;
+}
+
+// Theater-marquee-style chasing lights. Pass in a color (32-bit value,
+// a la strip.Color(r,g,b) as mentioned above), and a delay time (in ms)
+// between frames.
+/*#define THEATER_SPACING (LED_COUNT/20)
+uint8_t theaterChase(uint32_t color, int wait) {
+  for(uint8_t a=0; a<10; a++) {  // Repeat 10 times...
+    for(uint8_t b=0; b < THEATER_SPACING; b++) { //  'b' counts from 0 to 2...
+      strip.clear();         //   Set all pixels in RAM to 0 (off)
+      // 'c' counts up from 'b' to end of strip in steps of 3...
+      for(int c=b; c<LED_COUNT; c += 3) {
+        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+      }
+      strip.show(); // Update strip with new contents
+      delay(wait);  // Pause for a moment
+      sensors_event_t  event = getAmbientConditions ();
+      if (event.acceleration.z < 0){
+        stripOff ();
+        return 0;
+      }
+    }
+  }
+  return 1;
+}
+*/
+
+uint8_t twinkle(uint32_t color, int wait) {
+  for(int a=0; a<10; a++) {  // Repeat 10 times...
+    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
+      strip.clear();         //   Set all pixels in RAM to 0 (off)
+      // 'c' counts up from 'b' to end of strip in steps of 3...
+      for(int c=b; c<strip.numPixels(); c += 3) {
+        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+      }
+      strip.show(); // Update strip with new contents
+      delay(wait);  // Pause for a moment
+    }
+  }
+  return 1;
+}
+
+//Theatre-style crawling lights.
+#define THEATER_SPACING (LED_COUNT/3)
+uint8_t theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < THEATER_SPACING; q++) {
+      for (int i=0; i < LED_COUNT; i+=THEATER_SPACING) {
+        strip.setPixelColor(i+q, c);    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+      sensors_event_t  event = getAmbientConditions ();
+      if (event.acceleration.z < 0){
+        stripOff ();
+        return 0;
+      }
+
+      for (int i=0; i < LED_COUNT; i+=THEATER_SPACING) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+      
+    }
+  }
+  return 1;
+}
+
+// FuzzyRainbow cycle along whole strip. Pass delay time (in ms) between frames.
+uint8_t fuzzyRainbow(int wait) {
+  // Hue of first pixel runs 5 complete loops through the color wheel.
+  // Color wheel has a range of 65536 but it's OK if we roll over, so
+  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
+  // means we'll make 5*65536/256 = 1280 passes through this outer loop:
+  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
+    
+    for(int i=0; i<LED_COUNT; i++) { // For each pixel in strip...
+      // Offset pixel hue by an amount to make one full revolution of the
+      // color wheel (range of 65536) along the length of the strip
+      // (LED_COUNT steps):
+      int pixelHue = firstPixelHue + ((random(256)/255) * 65536L / LED_COUNT);
+      // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
+      // optionally add saturation and value (brightness) (each 0 to 255).
+      // Here we're using just the single-argument hue variant. The result
+      // is passed through strip.gamma32() to provide 'truer' colors
+      // before assigning to each pixel:
+      strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
+    }
+    strip.show(); // Update strip with new contents
+    delay(wait);  // Pause for a moment
+    sensors_event_t  event = getAmbientConditions ();
+    if (event.acceleration.z < 0){
+      stripOff ();
+      return 0;
+    }
+  }
+  return 1;
+}
+
+// Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
+uint8_t rainbow(int wait) {
+  // Hue of first pixel runs 5 complete loops through the color wheel.
+  // Color wheel has a range of 65536 but it's OK if we roll over, so
+  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
+  // means we'll make 5*65536/256 = 1280 passes through this outer loop:
+  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
+    
+    for(int i=0; i<LED_COUNT; i++) { // For each pixel in strip...
+      // Offset pixel hue by an amount to make one full revolution of the
+      // color wheel (range of 65536) along the length of the strip
+      // (LED_COUNT steps):
+      int pixelHue = firstPixelHue + (i * 65536L / LED_COUNT);
+      // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
+      // optionally add saturation and value (brightness) (each 0 to 255).
+      // Here we're using just the single-argument hue variant. The result
+      // is passed through strip.gamma32() to provide 'truer' colors
+      // before assigning to each pixel:
+      strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
+    }
+    strip.show(); // Update strip with new contents
+    delay(wait);  // Pause for a moment
+    sensors_event_t  event = getAmbientConditions ();
+    if (event.acceleration.z < 0){
+      stripOff ();
+      return 0;
+    }
+  }
+  return 1;
+}
+
+#define COLOR_CYCLE 65536 / 90; // One cycle of color wheel over 90 frames
+// Rainbow-enhanced theater marquee. Pass delay time (in ms) between frames.
+uint8_t theaterChaseRainbow(int wait) {
+  int firstPixelHue = 0;     // First pixel starts at red (hue 0)
+  for(uint8_t a=0; a<30; a++) {  // Repeat 30 times...
+    for(uint8_t b=0; b<3; b++) { //  'b' counts from 0 to 2...
+      strip.clear();         //   Set all pixels in RAM to 0 (off)
+      // 'c' counts up from 'b' to end of strip in increments of 3...
+      for(int c=b; c<LED_COUNT; c += 3) {
+        // hue of pixel 'c' is offset by an amount to make one full
+        // revolution of the color wheel (range 65536) along the length
+        // of the strip (LED_COUNT steps):
+        int      hue   = firstPixelHue + c * 65536L / LED_COUNT;
+        uint32_t color = strip.gamma32(strip.ColorHSV(hue)); // hue -> RGB
+        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+      }
+      firstPixelHue += COLOR_CYCLE;
+      strip.show();                // Update strip with new contents
+      delay(wait);                 // Pause for a moment
+      sensors_event_t  event = getAmbientConditions ();
+      if (event.acceleration.z < 0){
+        stripOff ();
+        return 0;
+      }
+    }
+  }
+  return 1;
+}
+
+
+uint8_t colorZapCycle (int wait)   {
+  int on = 0;
+  static int next = 0;
+  static int red = 0;
+  static int green = 0;
+  static int blue = 0;
+  int color = 0;  
+  switch (next) {
+  case 0:
+    red = ++red%256;
+    if (0 == red){
+      next = 1;
+      green = 1; 
+    }
+    break;
+  case 1:
+    green = ++green%256;
+    if (0 == green){
+      next = 2;
+      blue = 1; 
+    }
+    break;
+  case 2:
+    blue = ++blue%256;
+    if (0 == blue){
+      next = 0;
+      red = 1; 
+    }
+    break;
+  }
+  // Fill along the length of the strip in various colors...
+  return colorZap (strip.Color(red, green, blue), wait);
+}
+
+uint8_t colorSeqCycle (int wait) {
+  static int red = 127;
+  static int green = 0;
+  static int blue = 85;
+
+  if (blue < 85) {
+    red = (3+red)%256;
+    if (0 == red){
+      green = 1; 
+    }
+  }
+  if (red > 85) {
+    green = (3+green)%256;
+    if (0 == green){
+      blue = 1; 
+    }
+  }  
+  if (green > 85) {
+    blue = (3+blue)%256;
+    if (0 == blue){
+      red = 1; 
+    }
+  }
+  // Fill along the length of the strip in various colors...
+  return theaterChase (strip.Color(red, green, blue), wait);
+  
+}
+
+uint8_t colorWipeCycle (int wait) {
+  static int red = 0;
+  static int green = 0;
+  static int blue = 0;
+
+  if (blue < 85) {
+    red = (3+red)%256;
+//    if (0 == red){
+//      green = 1; 
+//    }
+  }
+  if (red > 85) {
+    green = (3+green)%256;
+//    if (0 == green){
+//      blue = 1; 
+//    }
+  }  
+  if (green > 85) {
+    blue = (3+blue)%256;
+//    if (0 == blue){
+//      red = 1; 
+//    }
+  }
+  // Fill along the length of the strip in various colors...
+  return colorWipe (strip.Color(red, green, blue), wait);
+}
+
+// List of animations speeds (in milliseconds).  This is how long an animation spends before
+// changing to the next step.  Higher values are slower.
+const uint16_t speeds[5] = { 400, 200, 100, 50, 25 };
+
+// Global state used by the sketch:
+uint8_t colorIndex = 0;
+uint8_t animationIndex = 0;
+uint8_t speedIndex = 2;
+
+uint8_t lanternLightPulsing (int wait) {
+  for (int i = 0; i < LED_COUNT; ++i) {
+    // Animation 0, solid color pulse of all pixels.
+    uint8_t currentStep = (millis()/speeds[speedIndex])%(colorSteps*2-2);
+    if (currentStep >= colorSteps) {
+      currentStep = colorSteps-(currentStep-(colorSteps-2));
+    }
+    // Note that colors are stored in flash memory so they need to be read
+    // using the pgmspace.h functions.
+    uint32_t color = pgm_read_dword_near(&colorPalette[colorIndex][currentStep]);
+    strip.setPixelColor(i, color);
+ }
+  strip.show();                // Update strip with new contents
+  delay(wait);                 // Pause for a moment
+  sensors_event_t  event = getAmbientConditions ();
+  if (event.acceleration.z < 0){
+    stripOff ();
+    return 0;
+  }
+  return 1;
+}
+
+
+uint8_t lanternLightModulate (int wait){
+  for (int i = 0; i < LED_COUNT; ++i) {  
+    // Animation 1, moving color pulse.  Use position to change brightness.
+    uint8_t currentStep = (millis()/speeds[speedIndex]+i)%(colorSteps*2-2);
+    if (currentStep >= colorSteps) {
+      currentStep = colorSteps-(currentStep-(colorSteps-2));
+    }
+    // Note that colors are stored in flash memory so they need to be read
+    // using the pgmspace.h functions.
+    uint32_t color = pgm_read_dword_near(&colorPalette[colorIndex][currentStep]);
+    strip.setPixelColor(i, color);
+  }
+  strip.show();                // Update strip with new contents
+  delay(wait);                 // Pause for a moment
+  sensors_event_t  event = getAmbientConditions ();
+  if (event.acceleration.z < 0){
+    stripOff ();
+    return 0;
+  }
+  return 1;
+
+}
+/*
+ * Serial Debug
+ */
+void setupSerial () {
+  Serial.begin(9600);
+  // Serial.setDebugOutput(true);
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  
+  // wait for serial monitor to open
+  while (!Serial);
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW  
+  Serial.print("Serial Com Ready!");
+}
+
+
+void setupAccelSensor () {
+
+  Serial.println("LIS3DH test!");
+
+  if (! lis.begin(0x18)) {   // change this to 0x19 for alternative i2c address
+    Serial.println("Couldnt start");
+    while (1) yield();
+  }
+  Serial.println("LIS3DH found!");
+
+  lis.setRange(LIS3DH_RANGE_4_G);   // 2, 4, 8 or 16 G!
+
+  Serial.print("Range = "); Serial.print(2 << lis.getRange());
+  Serial.println("G");
+
+  // lis.setDataRate(LIS3DH_DATARATE_50_HZ);
+  Serial.print("Data rate set to: ");
+  switch (lis.getDataRate()) {
+    case LIS3DH_DATARATE_1_HZ: Serial.println("1 Hz"); break;
+    case LIS3DH_DATARATE_10_HZ: Serial.println("10 Hz"); break;
+    case LIS3DH_DATARATE_25_HZ: Serial.println("25 Hz"); break;
+    case LIS3DH_DATARATE_50_HZ: Serial.println("50 Hz"); break;
+    case LIS3DH_DATARATE_100_HZ: Serial.println("100 Hz"); break;
+    case LIS3DH_DATARATE_200_HZ: Serial.println("200 Hz"); break;
+    case LIS3DH_DATARATE_400_HZ: Serial.println("400 Hz"); break;
+
+    case LIS3DH_DATARATE_POWERDOWN: Serial.println("Powered Down"); break;
+    case LIS3DH_DATARATE_LOWPOWER_5KHZ: Serial.println("5 Khz Low Power"); break;
+    case LIS3DH_DATARATE_LOWPOWER_1K6HZ: Serial.println("16 Khz Low Power"); break;
+  }
+  // 0 = turn off click detection & interrupt
+  // 1 = single click only interrupt output
+  // 2 = double click only interrupt output, detect single click
+  // Adjust threshhold, higher numbers are less sensitive
+  lis.setClick(2, CLICKTHRESHHOLD);
+  delay(100);    
+}
+
+void setupNeoPixel () {
+  // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
+  // Any other board, you can remove this part (but no harm leaving it):
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+#endif
+  // END of Trinket-specific code.
+
+  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // Turn OFF all pixels ASAP
+  strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
+ 
+}
+void setup() {
+  // if analog input pin 0 is unconnected, random analog
+  // noise will cause the call to randomSeed() to generate
+  // different seed numbers each time the sketch runs.
+  // randomSeed() will then shuffle the random function.
+  randomSeed(analogRead(0));
+ setupSerial ();
+ setupAccelSensor ();
+ setupNeoPixel ();
+}
+
+
+void runLightingEffect () {
+  static int mode = 0;
+  static int on = 1;
+  static int redFactor = 121;
+  static int greenFactor = 121;
+  static int blueFactor = 121;
+  // Serial.print("mode:  "); Serial.print(mode); Serial.print(", on:  "); Serial.print(on);
+  if (on == 0){
+    sensors_event_t  event = getAmbientConditions (); 
+    if (event.acceleration.z >= 0){
+      mode = (++mode % 12);
+      on = 1;
+    }
+  }
+  if (on == 0) return; 
+ 
+  switch (mode) {
+   case 1: 
+    // Fill along the length of the strip in various colors...
+    on = colorZapCycle(50);
+    break;
+  case 0:
+    on = colorSeqCycle (50);
+    break;
+  case 11:
+    on = colorWipeCycle(0); // Green
+    break;
+  case 3:
+    on = colorWipe(strip.Color(  0,   0, 255), 50); // Blue
+    break;
+  case 2:
+    on = lanternLightPulsing (50);
+    break;
+  case 10:
+    on = lanternLightModulate (50);
+    break;
+  case 9:
+   on = fuzzyRainbow (50);
+   break;
+  case 8:
+    // Use Acclerometer to modify color strip
+    {
+      sensors_event_t  event = getAmbientConditions ();
+  
+      redFactor += event.acceleration.x;
+      if (redFactor < 0) redFactor =1;
+      if (redFactor > 122) redFactor =122;
+       
+      greenFactor += event.acceleration.y;
+      if (greenFactor < 0) greenFactor =1;
+      if (greenFactor > 122) greenFactor = 122;
+       
+      blueFactor += (event.acceleration.z-10);
+      if (blueFactor < 0) blueFactor =1;
+      if (blueFactor > 122) blueFactor = 122;
+      
+      Serial.print("mode:  "); Serial.print(mode);
+      Serial.print(", redFactor:  "); Serial.print(redFactor);
+      Serial.print(", greenFactor:  "); Serial.print(greenFactor);
+      Serial.print(", blueFactor:  "); Serial.print(blueFactor);
+      Serial.println(); 
+      on = colorZap(strip.Color(1+(2*redFactor), 1+(2*greenFactor), 1+(2*blueFactor)), 5);
+      
+    }
+    break;
+  case 4: 
+    // Do a theater marquee effect in various colors...
+    on = theaterChase(strip.Color(255,   0,   0), 50); // Red, half brightness
+    break;
+  case 5:
+    // Do a theater marquee effect in various colors...
+    on = theaterChase(strip.Color(  0,   0, 127), 50); // Blue, half brightness
+    break;
+  case 6:
+    on = rainbow(10);             // Flowing rainbow cycle along the whole strip
+    break;
+  case 7:
+    on = theaterChaseRainbow(50); // Rainbow-enhanced theaterChase variant
+    break;
+  }  
+}
+
+uint8_t clickDetect (sensors_event_t & event) {
+  static uint8_t mode = 0;
+  static int z_orientation = 1;
+  uint8_t click = lis.getClick();
+  if ((click != 0) && (click & 0x30)){
+    Serial.print("Click detected (0x"); Serial.print(click, HEX); Serial.print("): ");
+    if (click & 0x10) {
+      Serial.print(" single click");
+     // ++mode;
+    }
+    if (click & 0x20) {
+      Serial.print(" double click");
+     // --mode;
+    }
+  } else {
+    if (event.acceleration.z <0) {
+      if (z_orientation > 0) {
+       z_orientation = -1;     
+       stripOff (); 
+      }
+    } else {
+      if (z_orientation < 0) {
+       z_orientation = 1;     
+       ++mode;
+      }          
+    }
+  }
+    Serial.print("zOrientation ("); Serial.print(z_orientation); Serial.print("): ");
+    // keep the mode between 0 and 7
+    mode = mode %8;
+    Serial.println();
+  
+  return mode;  
+}
+
+void loop() {
+
+  digitalWrite(ledPin, HIGH);
+  runLightingEffect ();
+  // turn the ledPin on
+  // turn the ledPin off:
+  digitalWrite(ledPin, LOW);
+ 
+}
---
+++ config.h
@@ -0,0 +1,92 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_LIS3DH.h>
+#include <Adafruit_Sensor.h>
+
+// Used for software SPI
+#define LIS3DH_CLK 13
+#define LIS3DH_MISO 12
+#define LIS3DH_MOSI 11
+// Used for hardware & software SPI
+#define LIS3DH_CS 10
+
+// Adjust this number for the sensitivity of the 'click' force
+// this strongly depend on the range! for 16G, try 5-10
+// for 8G, try 10-20. for 4G try 20-40. for 2G try 40-80
+#define CLICKTHRESHHOLD 40
+
+
+// NEOPIXEL BEST PRACTICES for most reliable operation:
+// - Add 1000 uF CAPACITOR between NeoPixel strip's + and - connections.
+// - MINIMIZE WIRING LENGTH between microcontroller board and first pixel.
+// - NeoPixel strip's DATA-IN should pass through a 300-500 OHM RESISTOR.
+// - AVOID connecting NeoPixels on a LIVE CIRCUIT. If you must, ALWAYS
+//   connect GROUND (-) first, then +, then data.
+// - When using a 3.3V microcontroller with a 5V-powered NeoPixel strip,
+//   a LOGIC-LEVEL CONVERTER on the data line is STRONGLY RECOMMENDED.
+// (Skipping these may work OK on your workbench but can fail in the field)
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1:
+#define LED_PIN    6
+
+// How many NeoPixels are attached to the Arduino?
+#define LED_COUNT 68
+
+// Declare our NeoPixel strip object:
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+// Argument 1 = Number of pixels in NeoPixel strip
+// Argument 2 = Arduino pin number (most are valid)
+// Argument 3 = Pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+
+// Build lookup table/palette for the color animations so they aren't computed at runtime.
+// The colorPalette two-dimensional array below has a row for each color animation and a column
+// for each step within the animation.  Each value is a 24-bit RGB color.  By looping through
+// the columns of a row the colors of pixels will animate.
+const int colorSteps = 8;   // Number of rows/animations.
+const int colorCount = 24;  // Number of columns/steps.
+const uint32_t colorPalette[colorCount][colorSteps] PROGMEM = {
+  // Complimentary colors  
+  { 0xFF0000, 0xDA2424, 0xB64848, 0x916D6D, 0x6D9191, 0x48B6B6, 0x24DADA, 0x00FFFF }, // Red-cyan
+  { 0xFFFF00, 0xDADA24, 0xB6B648, 0x91916D, 0x6D6D91, 0x4848B6, 0x2424DA, 0x0000FF }, // Yellow-blue
+  { 0x00FF00, 0x24DA24, 0x48B648, 0x6D916D, 0x916D91, 0xB648B6, 0xDA24DA, 0xFF00FF }, // Green-magenta
+
+  // Adjacent colors (on color wheel).
+  { 0xFF0000, 0xFF2400, 0xFF4800, 0xFF6D00, 0xFF9100, 0xFFB600, 0xFFDA00, 0xFFFF00 }, // Red-yellow
+  { 0xFFFF00, 0xDAFF00, 0xB6FF00, 0x91FF00, 0x6DFF00, 0x48FF00, 0x24FF00, 0x00FF00 }, // Yellow-green
+  { 0x00FF00, 0x00FF24, 0x00FF48, 0x00FF6D, 0x00FF91, 0x00FFB6, 0x00FFDA, 0x00FFFF }, // Green-cyan
+  { 0x00FFFF, 0x00DAFF, 0x00B6FF, 0x0091FF, 0x006DFF, 0x0048FF, 0x0024FF, 0x0000FF }, // Cyan-blue
+  { 0x0000FF, 0x2400FF, 0x4800FF, 0x6D00FF, 0x9100FF, 0xB600FF, 0xDA00FF, 0xFF00FF }, // Blue-magenta
+  { 0xFF00FF, 0xFF00DA, 0xFF00B6, 0xFF0091, 0xFF006D, 0xFF0048, 0xFF0024, 0xFF0000 }, // Magenta-red
+
+  // Other combos.
+  { 0xFF0000, 0xDA2400, 0xB64800, 0x916D00, 0x6D9100, 0x48B600, 0x24DA00, 0x00FF00 }, // Red-green
+  { 0xFFFF00, 0xDAFF24, 0xB6FF48, 0x91FF6D, 0x6DFF91, 0x48FFB6, 0x24FFDA, 0x00FFFF }, // Yellow-cyan
+  { 0x00FF00, 0x00DA24, 0x00B648, 0x00916D, 0x006D91, 0x0048B6, 0x0024DA, 0x0000FF }, // Green-blue
+  { 0x00FFFF, 0x24DAFF, 0x48B6FF, 0x6D91FF, 0x916DFF, 0xB648FF, 0xDA24FF, 0xFF00FF }, // Cyan-magenta
+  { 0x0000FF, 0x2400DA, 0x4800B6, 0x6D0091, 0x91006D, 0xB60048, 0xDA0024, 0xFF0000 }, // Blue-red
+  { 0xFF00FF, 0xFF24DA, 0xFF48B6, 0xFF6D91, 0xFF916D, 0xFFB648, 0xFFDA24, 0xFFFF00 }, // Magenta-yellow  
+
+  // Solid colors fading to dark.
+  { 0xFF0000, 0xDF0000, 0xBF0000, 0x9F0000, 0x7F0000, 0x5F0000, 0x3F0000, 0x1F0000 }, // Red
+  { 0xFF9900, 0xDF8500, 0xBF7200, 0x9F5F00, 0x7F4C00, 0x5F3900, 0x3F2600, 0x1F1300 }, // Orange
+  { 0xFFFF00, 0xDFDF00, 0xBFBF00, 0x9F9F00, 0x7F7F00, 0x5F5F00, 0x3F3F00, 0x1F1F00 }, // Yellow
+  { 0x00FF00, 0x00DF00, 0x00BF00, 0x009F00, 0x007F00, 0x005F00, 0x003F00, 0x001F00 }, // Green
+  { 0x0000FF, 0x0000DF, 0x0000BF, 0x00009F, 0x00007F, 0x00005F, 0x00003F, 0x00001F }, // Blue
+  { 0x4B0082, 0x410071, 0x380061, 0x2E0051, 0x250041, 0x1C0030, 0x120020, 0x090010 }, // Indigo
+  { 0x8B00FF, 0x7900DF, 0x6800BF, 0x56009F, 0x45007F, 0x34005F, 0x22003F, 0x11001F }, // Violet
+  { 0xFFFFFF, 0xDFDFDF, 0xBFBFBF, 0x9F9F9F, 0x7F7F7F, 0x5F5F5F, 0x3F3F3F, 0x1F1F1F }, // White
+
+  // Rainbow colors.
+  { 0xFF0000, 0xFF9900, 0xFFFF00, 0x00FF00, 0x0000FF, 0x4B0082, 0x8B00FF, 0xFFFFFF }
+};
